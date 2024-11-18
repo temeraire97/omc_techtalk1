f47f8628c486d831ff766e983ef9f9e819bfbba9
calia07 <calia_ksh@naver.com>
2024-11-18 22:14:04 +0900

    [24/11/18] UPDATE 배송비 아이콘 업데이트 함수 for문 >>> forEach 변환 완료

--- add_item_to_cart.js
+++ add_item_to_cart.js
@@ -50,14 +50,17 @@ function add_item_to_cart(name, price) {
   calc_cart_total();
 }
 
-function update_shipping_icons() {
-  var buy_buttons = get_buy_buttons_dom();
-  for (var i = 0; i < buy_buttons.length; i++) {
-    var button = buy_buttons[i];
-    var item = button.item;
-    if (item.price + shopping_cart_total >= 20) button.show_free_shopping_icon();
+/**
+ * @description 배송비 아이콘 업데이트하는 함수
+ */
+function update_shipping_icons(shopping_cart_total) {
+  const buy_buttons = get_buy_buttons_dom();
+
+  buy_buttons.forEach((button) => {
+    const item = button.item;
+    if(item.price+shopping_cart_total>=20) button.show_free_shopping_icon();
     else button.hide_free_shopping_icon();
-  }
+  });
 }
 
 function update_tax_dom() {
@@ -71,6 +74,6 @@ function calc_cart_total() {
     shopping_cart_total += item.price;
   }
   set_cart_total_dom();
-  update_shipping_icons();
+  update_shipping_icons(shopping_cart_total);
   update_tax_dom();
 }
